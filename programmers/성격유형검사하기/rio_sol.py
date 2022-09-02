af639b4ae3209f12b67846e41cce066c0da66ef7
honeyoung <dudgns5845@naver.com>
2022-09-03 00:20:48 +0900

    #2 Rio

--- programmers/성격유형검사하기/rio_sol.py
+++ programmers/성격유형검사하기/rio_sol.py
@@ -1,1 +1,33 @@
 # 코드 작성 테스트
+def solution(survey, choices):
+    answer = ''
+    board = {'R':0,'T':0,'J':0,'M':0,'A':0,'N':0,'C':0,'F':0}
+    
+    for idx, choice  in enumerate(choices):
+        if(choice == 4):
+            continue
+        elif (choice<4):
+            board[survey[idx][0]] += 4-choice
+        else:
+            board[survey[idx][1]] += choice - 4
+
+    if(board['R']>=board['T']):
+        answer += 'R'
+    else:
+        answer += 'T'
+
+    if(board['C']>=board['F']):
+        answer += 'C'
+    else:
+        answer += 'F'
+
+    if(board['J']>=board['M']):
+        answer += 'J'
+    else:
+        answer += 'M'
+
+    if(board['A']>=board['N']):
+        answer += 'A'
+    else:
+        answer += 'N'
+    return answer
